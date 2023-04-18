9865a42f7e66b92ddb48f44b44b1a4dde7dc771b
cuongtk <cuong.truongkien@gmail.com>
2023-04-18 23:00:28 +0700

    video23 props

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -1,11 +1,17 @@
 import React from "react";
 import UserInfor from "./UserInfor";
+import DisplayInfo from "./DisplayInfo";
 
 class MyComponent extends React.Component {
     render() {
+        const myInfor = ["ab", "c", "d"];
         return (
             <div>
                 <UserInfor />
+                <br />
+                <DisplayInfo name="Hoi Dan IT" age="30" />
+                <hr />
+                <DisplayInfo name={"Marcus"} age={33} myInfor={myInfor} />
             </div>
         );
     }
