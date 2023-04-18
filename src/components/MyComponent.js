6bb796e78b652df80417701bae80cec0c56b81ec
cuongtk <cuong.truongkien@gmail.com>
2023-04-18 11:29:48 +0700

    video16

---
+++ src/components/MyComponent.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class MyComponent extends React.Component {
+    render() {
+        return (
+            <div>
+                My first componenet
+                {Math.random()}
+            </div>
+        );
+    }
+}
+
+export default MyComponent;
