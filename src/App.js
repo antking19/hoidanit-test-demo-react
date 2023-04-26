e57a2a7aca84d2b7f8b5bfc72c14ca9e010d284b
antking19 <cuong.truongkien@gmail.com>
2023-04-27 00:17:07 +0700

    Video50 - Active Link - NavLink

--- src/App.js
+++ src/App.js
@@ -3,21 +3,21 @@ import Header from "./components/Header/Header";
 import { Outlet } from "react-router-dom";
 
 const App = () => {
-  return (
-    <div className="app-container">
-      <div className="header-container">
-        <Header />
-      </div>
+    return (
+        <div className="app-container">
+            <div className="header-container">
+                <Header />
+            </div>
 
-      <div className="main-container">
-        <div className="sidenav-container"></div>
-      </div>
+            <div className="main-container">
+                <div className="sidenav-container"></div>
+            </div>
 
-      <div className="app-content">
-        <Outlet />
-      </div>
-    </div>
-  );
+            <div className="app-content">
+                <Outlet />
+            </div>
+        </div>
+    );
 };
 
 export default App;
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,41 +2,47 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 const Header = () => {
-  return (
-    <Navbar bg="light" expand="lg">
-      <Container>
-        <Navbar.Brand href="#home">Hỏi Dân IT</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Link to="/" className="nav-link">
-              Home
-            </Link>
-            <Link to="/users" className="nav-link">
-              User
-            </Link>
-            <Link to="/admins" className="nav-link">
-              Admin
-            </Link>
-            {/* <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#user">Users</Nav.Link>
-            <Nav.Link href="#admin">Admin</Nav.Link> */}
-          </Nav>
-          <Nav>
-            <NavDropdown title="Setting" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Login</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">Sign in</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.3">Profile</NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-  );
+    return (
+        <Navbar bg="light" expand="lg">
+            <Container>
+                <Navbar.Brand href="#home">Hỏi Dân IT</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        <NavLink to="/" className="nav-link">
+                            Home
+                        </NavLink>
+                        <NavLink to="/users" className="nav-link">
+                            User
+                        </NavLink>
+                        <NavLink to="/admins" className="nav-link">
+                            Admin
+                        </NavLink>
+                        {/* <Nav.Link href="#home">Home</Nav.Link>
+                            <Nav.Link href="#user">Users</Nav.Link>
+                            <Nav.Link href="#admin">Admin</Nav.Link> */}
+                    </Nav>
+                    <Nav>
+                        <NavDropdown title="Setting" id="basic-nav-dropdown">
+                            <NavDropdown.Item href="#action/3.1">
+                                Login
+                            </NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.2">
+                                Sign in
+                            </NavDropdown.Item>
+                            <NavDropdown.Divider />
+                            <NavDropdown.Item href="#action/3.3">
+                                Profile
+                            </NavDropdown.Item>
+                        </NavDropdown>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
 };
 
 export default Header;
