b104d6bb749b5bd32d8ebb6fa7ea04e6fb9534fd
antking19 <cuong.truongkien@gmail.com>
2023-04-27 11:40:56 +0700

    Video52 - design homepage

--- src/components/Home/HomePage.js
+++ src/components/Home/HomePage.js
@@ -1,5 +1,13 @@
+import HomeVideo from "../../assets/video-1920.mp4";
+
 const HomePage = (props) => {
-    return <div>HomePage Component</div>;
+    return (
+        <div>
+            <video autoPlay muted loop>
+                <source src={HomeVideo} type="video/mp4" />
+            </video>
+        </div>
+    );
 };
 
 export default HomePage;
