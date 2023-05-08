1352dfa69e7cc17dc52ad497573005f72161f37a
antking19 <cuong.truongkien@gmail.com>
2023-05-08 19:04:11 +0700

    Video68 - Hien thi danh sach user phan trang - paginate

--- src/components/Admin/content/ManageUser.js
+++ src/components/Admin/content/ManageUser.js
@@ -2,13 +2,21 @@ import ModalCreateUser from "./ModalCreateUser";
 import "./ManageUser.scss";
 import { useEffect, useState } from "react";
 import { FcPlus } from "react-icons/fc";
-import TableUser from "./TableUser";
-import { getAllUsers } from "../../../services/apiServices";
+// import TableUser from "./TableUser";
+import {
+    getAllUsers,
+    getUserWithPaginate,
+} from "../../../services/apiServices";
 import ModalUpdateUser from "./ModalUpdateUser";
 import ModalViewUser from "./ModalViewUser";
 import ModalDeleteUser from "./ModalDeleteUser";
+import TableUserWithPaginate from "./TableUserWithPaginate";
 
 const ManageUser = (props) => {
+    const LIMIT_USER = 6;
+
+    const [pageCount, setPageCount] = useState(0);
+
     const [showModalCreateUser, setShowModalCreateUser] = useState(false);
     const [showModalUpdateUser, setShowModalUpdateUser] = useState(false);
     const [showModalViewUser, setShowModalViewUser] = useState(false);
@@ -21,13 +29,23 @@ const ManageUser = (props) => {
 
     // componentDidMount
     useEffect(() => {
-        fetchListUsers();
+        // fetchListUsers();
+        fetchListUsersWithPaginate(1);
     }, []);
 
     const fetchListUsers = async () => {
         let res = await getAllUsers();
         if (res && res.EC === 0) {
             setListUsers(res.DT);
+        }
+    };
+
+    const fetchListUsersWithPaginate = async (page) => {
+        let res = await getUserWithPaginate(page, LIMIT_USER);
+        console.log(res);
+        if (res && res.EC === 0) {
+            setListUsers(res.DT.users);
+            setPageCount(res.DT.totalPages);
         }
     };
 
@@ -64,11 +82,20 @@ const ManageUser = (props) => {
                     </button>
                 </div>
                 <div>
-                    <TableUser
+                    {/* <TableUser
                         listUsers={listUsers}
                         handleClickBtnUpdate={handleClickBtnUpdate}
                         handleClickBtnView={handleClickBtnView}
                         handleClickBtnDelete={handleClickBtnDelete}
+                    /> */}
+
+                    <TableUserWithPaginate
+                        listUsers={listUsers}
+                        handleClickBtnUpdate={handleClickBtnUpdate}
+                        handleClickBtnView={handleClickBtnView}
+                        handleClickBtnDelete={handleClickBtnDelete}
+                        fetchListUsersWithPaginate={fetchListUsersWithPaginate}
+                        pageCount={pageCount}
                     />
                 </div>
                 <ModalCreateUser
---
+++ src/components/Admin/content/TableUserWithPaginate.js
@@ -0,0 +1,97 @@
+import { useState, useEffect } from "react";
+import ReactPaginate from "react-paginate";
+
+const TableUserWithPaginate = (props) => {
+    const { listUsers, fetchListUsersWithPaginate, pageCount } = props;
+
+    const handlePageClick = (event) => {
+        fetchListUsersWithPaginate(+event.selected + 1);
+        console.log(`User requested page number ${event.selected}`);
+    };
+
+    return (
+        <>
+            <table className="table table-hover table-bordered">
+                <thead>
+                    <tr>
+                        <th scope="col">ID</th>
+                        <th scope="col">User Name</th>
+                        <th scope="col">Email</th>
+                        <th scope="col">Role</th>
+                        <th scope="col">Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {listUsers &&
+                        listUsers.length > 0 &&
+                        listUsers.map((item, index) => {
+                            return (
+                                <tr key={`table-users-${index}`}>
+                                    <th scope="row">{item.id}</th>
+                                    <td>{item.username}</td>
+                                    <td>{item.email}</td>
+                                    <td>{item.role}</td>
+                                    <td>
+                                        <button
+                                            className="btn btn-secondary"
+                                            onClick={() =>
+                                                props.handleClickBtnView(item)
+                                            }
+                                        >
+                                            View
+                                        </button>
+                                        <button
+                                            className="btn btn-warning mx-3"
+                                            onClick={() =>
+                                                props.handleClickBtnUpdate(item)
+                                            }
+                                        >
+                                            Update
+                                        </button>
+                                        <button
+                                            className="btn btn-danger"
+                                            onClick={() =>
+                                                props.handleClickBtnDelete(item)
+                                            }
+                                        >
+                                            Delete
+                                        </button>
+                                    </td>
+                                </tr>
+                            );
+                        })}
+
+                    {listUsers && listUsers.length === 0 && (
+                        <tr>
+                            <td colSpan={"4"}>Not found data</td>
+                        </tr>
+                    )}
+                </tbody>
+            </table>
+            <div className="d-flex justify-content-center">
+                <ReactPaginate
+                    nextLabel="Next >"
+                    onPageChange={handlePageClick}
+                    pageRangeDisplayed={3}
+                    marginPagesDisplayed={2}
+                    pageCount={pageCount}
+                    previousLabel="< Prev"
+                    pageClassName="page-item"
+                    pageLinkClassName="page-link"
+                    previousClassName="page-item"
+                    previousLinkClassName="page-link"
+                    nextClassName="page-item"
+                    nextLinkClassName="page-link"
+                    breakLabel="..."
+                    breakClassName="page-item"
+                    breakLinkClassName="page-link"
+                    containerClassName="pagination"
+                    activeClassName="active"
+                    renderOnZeroPageCount={null}
+                />
+            </div>
+        </>
+    );
+};
+
+export default TableUserWithPaginate;
