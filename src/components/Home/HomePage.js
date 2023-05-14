4069104462770ff7719c99291b493782e32f1dc7
antking19 <cuong.truongkien@gmail.com>
2023-05-14 07:17:34 +0700

    video 78 - su dung redux luu thong tin người dung

--- src/components/Home/HomePage.js
+++ src/components/Home/HomePage.js
@@ -1,6 +1,12 @@
 import HomeVideo from "../../assets/video-1920.mp4";
+import { useSelector } from "react-redux"; //hook lấy ra state của redux
 
 const HomePage = (props) => {
+    const isAuthenticated = useSelector((state) => state.user.isAuthenticated);
+    const account = useSelector((state) => state.user.account);
+
+    console.log("account: ", account, "isAuthenticated: ", isAuthenticated);
+
     return (
         <div>
             <video autoPlay muted loop>
