85c38bd88ea1460eaf42f6543e04e389fd69bfd2
antking19 <cuong.truongkien@gmail.com>
2023-05-14 19:55:02 +0700

    video78 su dung redux luu thong tin nguoi dung

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -3,18 +3,22 @@ import "./Login.scss";
 import { useNavigate } from "react-router-dom";
 import { postLogin } from "../../services/apiServices";
 import { toast } from "react-toastify";
+import { useDispatch } from "react-redux";
+import { doLogin } from "../../redux/action/userAction";
 
 const Login = (props) => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
 
     const navigate = useNavigate();
+    const dispatch = useDispatch();
 
     const handleLogin = async () => {
         const data = await postLogin(email, password);
         console.log(data);
 
         if (data && data.EC === 0) {
+            dispatch(doLogin(data));
             toast.success(data.EM);
             navigate("/");
         }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,15 @@
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-// import NavDropdown from "react-bootstrap/NavDropdown";
+import NavDropdown from "react-bootstrap/NavDropdown";
 import { NavLink, useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const Header = () => {
     const navigate = useNavigate();
+
+    const isAuthenticated = useSelector((state) => state.user.isAuthenticated);
+    const account = useSelector((state) => state.user.account);
 
     const handleLogin = () => {
         navigate("/login");
@@ -35,34 +39,37 @@ const Header = () => {
                             <Nav.Link href="#admin">Admin</Nav.Link> */}
                     </Nav>
                     <Nav>
-                        <div>
-                            <button
-                                className="btn btn-outline-dark mx-3 btn-login"
-                                onClick={() => handleLogin()}
+                        {isAuthenticated === false ? (
+                            <>
+                                <button
+                                    className="btn btn-outline-dark mx-3 btn-login"
+                                    onClick={() => handleLogin()}
+                                >
+                                    Login
+                                </button>
+                                <button
+                                    className="btn btn-dark btn-signup"
+                                    onClick={() => {
+                                        navigate("/register");
+                                    }}
+                                >
+                                    Sign Up
+                                </button>
+                            </>
+                        ) : (
+                            <NavDropdown
+                                title="Setting"
+                                id="basic-nav-dropdown"
                             >
-                                Login
-                            </button>
-                            <button
-                                className="btn btn-dark btn-signup"
-                                onClick={() => {
-                                    navigate("/register");
-                                }}
-                            >
-                                Sign Up
-                            </button>
-                        </div>
-                        {/* <NavDropdown title="Setting" id="basic-nav-dropdown">
-                            <NavDropdown.Item href="#action/3.1">
-                                Login
-                            </NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.2">
-                                Sign in
-                            </NavDropdown.Item>
-                            <NavDropdown.Divider />
-                            <NavDropdown.Item href="#action/3.3">
-                                Profile
-                            </NavDropdown.Item>
-                        </NavDropdown> */}
+                                <NavDropdown.Item href="#action/3.1">
+                                    Login
+                                </NavDropdown.Item>
+                                <NavDropdown.Divider />
+                                <NavDropdown.Item href="#action/3.3">
+                                    Profile
+                                </NavDropdown.Item>
+                            </NavDropdown>
+                        )}
                     </Nav>
                 </Navbar.Collapse>
             </Container>
